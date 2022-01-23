e653348d4c1524b7873e073e748e5daf2296cb1c
Yuriy Solovyov <dev@solovyov.de>
2022-01-23 22:33:46 +0100

    initial commit

---
+++ src/main/java/de/solovyov/spring/springbootstudenapp/SpringBootStudenAppApplication.java
@@ -0,0 +1,13 @@
+package de.solovyov.spring.springbootstudenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/solovyov/spring/springbootstudenapp/SpringBootStudenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.solovyov.spring.springbootstudenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
